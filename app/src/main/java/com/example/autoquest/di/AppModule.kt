fde1de8eb5a09009161fcf17ad132f991d026ccf
TkachenkoND00 <tkachenkond00@gmail.com>
2022-05-30 19:14:37 +0300

    create app module

---
+++ app/src/main/java/com/example/autoquest/di/AppModule.kt
@@ -0,0 +1,9 @@
+package com.example.autoquest.di
+
+import com.example.autoquest.presentation.view_model.QuestSharedViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+val viewModelModule = module {
+    viewModel { QuestSharedViewModel(get(),get(),get(),get(),get()) }
+}
