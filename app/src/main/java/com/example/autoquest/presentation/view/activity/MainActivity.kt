7c50e5121bf6073f18790e2f70192e0ab06c72bc
TkachenkoND00 <tkachenkond00@gmail.com>
2022-05-30 18:56:30 +0300

    created main activity

--- app/src/main/java/com/example/autoquest/presentation/view/activity/MainActivity.kt
+++ app/src/main/java/com/example/autoquest/presentation/view/activity/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.autoquest.presentation.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.fragment.app.commit
 import com.example.autoquest.R
+import com.example.autoquest.databinding.ActivityMainBinding
+import com.example.autoquest.presentation.view.fargment.ListOfQuestsFragment
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.commit {
+            replace(R.id.containerFragment, ListOfQuestsFragment())
+        }
+
+        AppCompatDelegate.setDefaultNightMode(
+            AppCompatDelegate.MODE_NIGHT_NO
+        )
     }
 }
---
+++ app/src/main/java/com/example/autoquest/presentation/view/fargment/ListOfQuestsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.autoquest.presentation.view.fargment
+
+import androidx.fragment.app.Fragment
+
+class ListOfQuestsFragment: Fragment() {
+}
