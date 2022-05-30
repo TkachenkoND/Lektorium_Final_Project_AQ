3955d96f164eeb70412b42b62789beebd5a01ece
TkachenkoND00 <tkachenkond00@gmail.com>
2022-05-30 19:12:49 +0300

    created helper in data module

---
+++ app/src/main/java/com/example/autoquest/data/helper/ConstVariables.kt
@@ -0,0 +1,5 @@
+package com.example.autoquest.data.helper
+
+const val APP_PREFERENCES = "mysettings"
+const val FLAG = "flag"
+const val QUEST_ID = "questId"
---
+++ app/src/main/java/com/example/autoquest/data/helper/ConversionHelper.kt
@@ -0,0 +1,48 @@
+package com.example.autoquest.data.helper
+
+import com.example.autoquest.data.database.entity.QuestItemEntity
+import com.example.autoquest.data.database.entity.QuestTaskEntity
+import com.example.autoquest.domain.models.QuestItem
+import com.example.autoquest.domain.models.QuestTask
+
+fun QuestTask.toQuestTaskEntity() = QuestTaskEntity(
+    questsId = questsId,
+    textTask = textTask,
+    answerTask = answerTask,
+    accessCode = accessCode,
+    latitude = latitude,
+    longitude = longitude
+)
+
+fun QuestTaskEntity.toQuestTask() = QuestTask(
+    questsId = questsId,
+    textTask = textTask,
+    answerTask = answerTask,
+    accessCode = accessCode,
+    latitude = latitude,
+    longitude = longitude
+)
+
+fun QuestItem.toQuestItemEntity() = QuestItemEntity(
+    questsId = questsId,
+    itemBackgroundImg = itemBackgroundImg,
+    rating = rating,
+    questDescription = questDescription,
+    dataQuest = dataQuest,
+    nameQuest = nameQuest,
+    timeQuest = timeQuest,
+    placeStartQuest = placeStartQuest,
+    imgDetailsQuest = imgDetailsQuest
+)
+
+fun QuestItemEntity.toQuestItem() = QuestItem(
+    questsId = questsId,
+    itemBackgroundImg = itemBackgroundImg,
+    rating = rating,
+    questDescription = questDescription,
+    dataQuest = dataQuest,
+    nameQuest = nameQuest,
+    timeQuest = timeQuest,
+    placeStartQuest = placeStartQuest,
+    imgDetailsQuest = imgDetailsQuest
+)
