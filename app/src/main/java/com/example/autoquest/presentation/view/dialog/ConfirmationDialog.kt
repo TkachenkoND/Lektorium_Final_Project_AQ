4da0e0bfd1a03cf1e6fcbd834050535fd165297c
TkachenkoND00 <tkachenkond00@gmail.com>
2022-05-30 19:37:42 +0300

    create dialog

---
+++ app/src/main/java/com/example/autoquest/presentation/view/dialog/ConfirmationDialog.kt
@@ -0,0 +1,29 @@
+package com.example.autoquest.presentation.view.dialog
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.DialogFragment
+import com.example.autoquest.R
+import com.example.autoquest.presentation.view_model.QuestSharedViewModel
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
+
+class ConfirmationDialog : DialogFragment() {
+
+    private val sharedVm by sharedViewModel<QuestSharedViewModel>()
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog =
+        AlertDialog.Builder(requireContext())
+            .setTitle(getString(R.string.title_dialog))
+            .setMessage(getString(R.string.messege_dialog))
+            .setPositiveButton(getString(R.string.positive_btn_txt_dialog)) { _, _ ->
+                sharedVm.clickBtnDialog(true)
+            }
+            .setNegativeButton(getString(R.string.negative_btn_txt_dialog)) { _, _ ->
+            }
+            .create()
+
+    companion object {
+        const val TAG = "ConfirmationDialog"
+    }
+}
