fea93f550dc3ab00da0cd0b00fa88e047d3f16b5
TkachenkoND00 <tkachenkond00@gmail.com>
2022-05-30 19:20:24 +0300

    create App Apli in di module

---
+++ app/src/main/java/com/example/autoquest/di/App.kt
@@ -0,0 +1,30 @@
+package com.example.autoquest.di
+
+import android.app.Application
+import com.example.autoquest.di.*
+import org.koin.android.BuildConfig
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.logger.Level
+
+class App: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        startKoin {
+            androidLogger(if (BuildConfig.DEBUG) Level.ERROR else Level.NONE)
+            androidContext(this@App)
+            modules(
+                listOf(
+                    networkModule,
+                    viewModelModule,
+                    dataModule,
+                    domainModule,
+                    dbModule
+                )
+            )
+        }
+    }
+}
